98623a481512ae684e4b5a766a43a6c39c9c4723
Pierre Pontet <ppontet@student.42lyon.fr>
2026-05-20 11:43:17 +0200

    feat: added dev and prod build for compose.yml

--- API-ObjectStorage/startup.sh
+++ API-ObjectStorage/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#Initialize the database with the required tables and schema
-node ace migration:run 
+#Initialize the database with the required tables and schema (--force needed with NODE_ENV=production)
+node ace migration:run --force
 
 if [ "$NODE_ENV" = "production" ]; then
   exec node bin/server.js
