ed200d9e613103ef6d1201dfe53a38acf47ad4b2
Pierre Pontet <ppontet@student.42lyon.fr>
2026-06-07 15:56:27 +0200

    feat: better Dockerfile with better stages

--- API-ObjectStorage/startup.sh
+++ API-ObjectStorage/startup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#Initialize the database with the required tables and schema (--force needed with NODE_ENV=production)
-node ace migration:run --force
+set -e
 
-if [ "$NODE_ENV" = "production" ]; then
-  exec node bin/server.js
-else
-  exec npm run dev
-fi
+#Initialize the database with the required tables and schema (--force needed with NODE_ENV=production)
+npm run migration-force
+
+exec "$@"
