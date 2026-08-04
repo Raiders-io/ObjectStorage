9ccb61df0eebde5996776cb0ce0a62aef40cb0b8
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-04 16:26:12 +0200

    feat: extracted logic from data_access_controller to use fx in mq

--- API-ObjectStorage/app/services/message_broker_service.ts
+++ API-ObjectStorage/app/services/message_broker_service.ts
@@ -1,24 +1,26 @@
 import { AsyncMessage } from 'rabbitmq-client'
+import { deleteAllObjectsForUser } from '#services/access_object_service'
 
 export async function handleAsyncMessage(msg: AsyncMessage) {
   switch (msg.routingKey) {
-    case 'auth.user.created':
-      console.log('user created event received')
-      break
+    // case 'auth.user.created':
+    //   console.log('user created event received')
+    //   break
     case 'auth.user.deleted':
       console.log('user deleted event received')
+      await deleteAllObjectsForUser(msg.body.userId)
       break
     default:
       console.log('unknown event received')
       return
   }
-  console.log('received message ', msg)
-  console.log('message content is ', msg.body)
+  // console.log('received message ', msg)
+  // console.log('message content is ', msg.body)
 }
 
 export async function handleLogMessage(str: String) {
   console.log({
-    time: `${new Date().toISOString()}`,
+    time: `${new Date()}`,
     msg: str,
   })
 }
