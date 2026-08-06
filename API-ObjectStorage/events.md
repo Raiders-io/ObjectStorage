180104cd6041cc648a418ce22672a65e905c8b27
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-06 18:31:49 +0200

    wip: specs for message broker service

---
+++ API-ObjectStorage/events.md
@@ -0,0 +1,78 @@
+# Events for the message broker service
+
+Events followed :
+
+- `auth.user.deleted`
+
+Events created :
+
+- `object.file.created`
+- `object.file.updated`
+- `object.file.deleted`
+- `object.file.visibility`
+- `object.user.data`
+
+## `object.file.created`
+
+Published when a file is created by a user.
+
+payload :
+
+```json
+{
+  file: file-id,
+  userId: user-id,
+}
+```
+
+## `object.file.updated`
+
+Published when a file is updated by a user.
+
+payload :
+
+```json
+{
+  file: file-id,
+  userId: user-id,
+}
+```
+
+## `object.file.deleted`
+
+Published when a file is deleted by a user.
+
+payload :
+
+```json
+{
+  file: file-id,
+  userId: user-id,
+}
+```
+
+## `object.file.visibility`
+
+Published when the visibility of a file is edited by a user.
+
+payload :
+
+```json
+{
+  file: file-id,
+  visibility: new-visiblity,
+}
+```
+
+## `object.user.data`
+
+Published when an action is performed on all data from a user.
+
+payload :
+
+```json
+{
+  userId: user-id,
+  state: requested | deleted,
+}
+```
---
+++ API-ObjectStorage/providers/message_broker_provider.ts
@@ -0,0 +1,51 @@
+import { handleAsyncMessage } from '#services/message_broker_service'
+import type { ApplicationService } from '@adonisjs/core/types'
+import { Broker, publish, consume } from '@yosone/broker'
+
+export default class MessageBrokerProvider {
+  constructor(protected app: ApplicationService) {}
+
+  /**
+   * Register bindings to the container
+   */
+  register() {
+  }
+
+  /**
+   * The container bindings have booted
+   */
+  async boot() {}
+
+  /**
+   * The application has been booted
+   */
+  async start() {
+    Broker.init({
+      redisUrl: process.env.REDIS_URL || 'redis://redis:6379',
+      group: "object-service",
+      consumer: "object-service",
+    })
+
+    publish('object.events', {
+      type: 'object.started',
+      payload: {
+        message: 'Object service has been started',
+      },
+    })
+  }
+  /**
+   * The process has been started
+   */
+  async ready() {
+    await consume('auth.events')
+      .on('auth.user.deleted', handleAsyncMessage)
+      .start()
+  }
+
+  /**
+   * Preparing to shutdown the app
+   */
+  async shutdown() {
+    Broker.disconnect()
+  }
+}
