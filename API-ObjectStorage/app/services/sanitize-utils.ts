12db3751980c8e8e11fb51020f6ada50bda27a07
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-13 00:41:59 +0200

    fix: removing console.log() in sanitizeFilename

--- API-ObjectStorage/app/services/sanitize-utils.ts
+++ API-ObjectStorage/app/services/sanitize-utils.ts
@@ -1,8 +1,6 @@
 export function sanitizeFilename(filename: string): string | undefined {
   // Keep only alphanumeric characters, underscores, hyphens, and dots in the filename
-  console.log(`Sanitizing filename: ${filename}`)
   const sanitized = filename.replace(/[^a-zA-Z0-9_\-\.]/g, '');
-  console.log(`Sanitized filename: ${sanitized}`)
   if (sanitized.length === 0 || sanitized === '.' || sanitized === '..')
     return undefined; // Return undefined if the sanitized filename is empty
 
