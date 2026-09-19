7d65690bf534908ebc89f3b796d7a8a53a3c9985
Pierre Pontet <ppontet@student.42lyon.fr>
2026-09-19 17:03:33 +0200

    feat: typecheck of msg using zod working

--- API-ObjectStorage/adonisrc.ts
+++ API-ObjectStorage/adonisrc.ts
@@ -53,7 +53,10 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('#providers/api_provider'),
     () => import('@adonisjs/drive/drive_provider'),
-    () => import('#providers/message_broker_provider'),
+    {
+      file: () => import('#providers/message_broker_provider'),
+      environment: ['web'],
+    },
   ],
 
   /*
